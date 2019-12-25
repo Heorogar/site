ff3a1e8ab48e7563cadea04f13195a70a5ffa81d
alek <alek.businessfon@gmail.com>
2019-12-25 12:22:25 +0100

    deleted testing script

--- skripta.js
+++
@@ -1,7 +0,0 @@
-function validateInput(){
-  let forms=document.getElementsByTagName("input");
-  document.write('foo');
-  if(forms[2].value="cpu")
-  alert("foo");
-}
-document.getElementById('submit').addEventListener('click',validateInput);
