387175172d5bad634b9fc530ca6d4adcc9f37d90
alek <alek.businessfon@gmail.com>
2019-12-25 12:19:25 +0100

    implemented victory screen

--- js/skripta.js
+++ js/skripta.js
@@ -4,7 +4,19 @@ function validateInput() {
     "gnu");
   let checkboxes = document.getElementsByName("checkboxes");
 
-  if ((forms.clock.value.toLowerCase() == "cpu" || forms.clock.value.toLowerCase() == "central processing unit") && forms.mephisto.value.toLowerCase() == "documentation" && forms.dungeon.value.toLowerCase() == "linux" && radios[1].checked && checkboxes[0].checked == false && checkboxes[1].checked == false && checkboxes[2].checked && checkboxes[3].checked)
-    alert("foo");
+  if ((forms.clock.value.toLowerCase() == "cpu" || forms.clock.value.toLowerCase() == "central processing unit") && forms.mephisto.value.toLowerCase() == "documentation" && forms.dungeon.value.toLowerCase() == "linux" && radios[1].checked && checkboxes[0].checked == false && checkboxes[1].checked == false && checkboxes[2].checked && checkboxes[3].checked) {
+    $('p').hide();
+    $('#submit').hide();
+    $("#knight").show();
+    $("#reward").show();
+    $('html').animate({
+      scrollTop: 0
+    }, 5000);
+  } else
+    alert("Wrong thou art, try thee again!");
 }
 document.getElementById("submit").addEventListener("click", validateInput);
+$(document).ready(function() {
+  $("#reward").hide();
+  $("#knight").hide();
+});
