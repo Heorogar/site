2406e2dad85523beefafe1d17ab06385d2b81e58
alek <alek.businessfon@gmail.com>
2020-01-05 21:50:44 +0100

    hopefully provided more informative browser client browser information

--- js/menu_info.js
+++ js/menu_info.js
@@ -1,7 +1,7 @@
 /*jshint esversion: 6 */
 document.getElementById('index_info').innerHTML += 'Platform: ' + navigator.platform;
 
-document.getElementById('index_info').innerHTML += '<br><br> Browser: ' + navigator.appName;
+document.getElementById('index_info').innerHTML += '<br><br> Browser: ' + navigator.appCodeName;
 
 let date = new Date();
 document.getElementById('index_info').innerHTML += '<br><br> Date: ' + date.getDate() + '. ' + date.getMonth() + '. ' + date.getFullYear() + '.';
